0edef42829ae79f5417c4fcec7ab5ec3c13db447
Stanislav Reva <RevaSA95@gmail.com>
2020-04-18 22:15:12 +0300

    Add links and dropdown

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,22 +10,22 @@
 
             <ul class="right hide-on-small-and-down">
                 <li>
-                    <a href="#" class="dropdown-trigger black-text" data-target="dropdown">
+                    <a href="#" class="dropdown-trigger black-text" data-target="dropdown" ref="dropdown">
                         USER NAME
                         <i class="material-icons right">arrow_drop_down</i>
                     </a>
 
                     <ul id='dropdown' class='dropdown-content'>
                         <li>
-                            <a href="#" class="black-text">
+                            <router-link class="black-text" to="/profile">
                                 <i class="material-icons">account_circle</i> Профиль
-                            </a>
+                            </router-link>
                         </li>
 
                         <li class="divider" tabindex="-1"></li>
 
                         <li>
-                            <a href="#" class="black-text">
+                            <a href="#" class="black-text" @click.prevent="logout">
                                 <i class="material-icons">assignment_return</i> Выйти
                             </a>
                         </li>
@@ -35,3 +35,21 @@
         </div>
     </nav>
 </template>
+
+<script>
+    import M from 'materialize-css';
+
+    export default {
+        methods: {
+            logout() {
+                console.log('logout');
+                this.$router.push('/login?message=logout');
+            },
+        },
+        mounted() {
+            M.Dropdown.init(this.$refs.dropdown, {
+                alignment: 'right',
+            });
+        },
+    };
+</script>
