dfa14f1caa71c5e51186ad6346a4797daec25da5
Stanislav Reva <RevaSA95@gmail.com>
2020-04-18 18:55:15 +0300

    Make navbar and sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,45 @@
+<template>
+    <ul class="sidenav app-sidenav" :class="{ open: value }">
+        <router-link
+            v-for="link in links"
+            tag="li"
+            active-class="active"
+            :to="link.url"
+            :exact="link.exact"
+            :key="link.url"
+        >
+            <a class="waves-effect waves-orange pointer">{{link.title}}</a>
+        </router-link>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+        data: () => ({
+            links: [
+                {
+                    title: 'Счет',
+                    url: '/',
+                    exact: true,
+                },
+                {
+                    title: 'История',
+                    url: '/history',
+                },
+                {
+                    title: 'Планирование',
+                    url: '/planning',
+                },
+                {
+                    title: 'Новая запись',
+                    url: '/record',
+                },
+                {
+                    title: 'Категории',
+                    url: '/categories',
+                },
+            ],
+        }),
+    };
+</script>
