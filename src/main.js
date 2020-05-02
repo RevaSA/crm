6b811bb333cb5eebf439e20099a102ac2c665a6d
Stanislav Reva <RevaSA95@gmail.com>
2020-05-02 14:46:53 +0300

    Move firebase initialize

--- src/main.js
+++ src/main.js
@@ -5,9 +5,18 @@ import router from '@/router';
 import store from '@/store';
 import messagePlugin from '@/utils/message';
 import Loader from '@/components/Loader';
-import '@/firebase';
 import '@/registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min';
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyAfgbnGIMaQwJmTH6tWNwqJbDFYJ54CDGE',
+    authDomain: 'dementor-vue-crm.firebaseapp.com',
+    databaseURL: 'https://dementor-vue-crm.firebaseio.com',
+    projectId: 'dementor-vue-crm',
+    storageBucket: 'dementor-vue-crm.appspot.com',
+    messagingSenderId: '287515160947',
+    appId: '1:287515160947:web:b9648413839ac65aad9506',
+});
 
 Vue.config.productionTip = false;
 Vue.use(messagePlugin);
