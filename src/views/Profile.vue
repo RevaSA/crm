d706cdead29abf4964e2a06ea6d600947e02840b
Stanislav Reva <RevaSA95@gmail.com>
2020-05-10 20:50:43 +0300

    Make profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,17 +4,64 @@
             <h3>Профиль</h3>
         </div>
 
-        <form class="form">
+        <form class="form" @submit.prevent="submitHandler">
             <div class="input-field">
-                <input id="description" type="text">
+                <input
+                    id="description"
+                    type="text"
+                    v-model="name"
+                    :class="{invalid: $v.name.$dirty && !$v.name.required}"
+                >
+
                 <label for="description">Имя</label>
-                <span class="helper-text invalid">name</span>
+
+                <small class="helper-text invalid" v-if="$v.name.$dirty && !$v.name.required">
+                    Введите имя
+                </small>
             </div>
 
-            <button class="btn waves-effect waves-light">
+            <button class="btn waves-effect waves-light" type="submit">
                 Обновить
                 <i class="material-icons right">send</i>
             </button>
         </form>
     </div>
 </template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    import { validationMixin } from 'vuelidate';
+    import { required } from 'vuelidate/lib/validators';
+
+    export default {
+        mixins: [validationMixin],
+        data: () => ({
+            name: '',
+        }),
+        computed: {
+            ...mapGetters(['info']),
+        },
+        methods: {
+            async submitHandler() {
+                if (this.$v.$invalid) {
+                    this.$v.$touch();
+                    return;
+                }
+
+                await this.$store.dispatch('updateInfo', {
+                    name: this.name,
+                });
+            },
+        },
+        mounted() {
+            this.name = this.info.name;
+
+            this.$nextTick(() => {
+                window.M.updateTextFields();
+            });
+        },
+        validations: {
+            name: { required },
+        },
+    };
+</script>
