e4f09674356ef232c5c2fe7b4c38aaf6b7666703
Stanislav Reva <RevaSA95@gmail.com>
2020-05-11 14:43:11 +0300

    Update currency url

--- src/store/currency.js
+++ src/store/currency.js
@@ -1,10 +1,12 @@
 export default {
     actions: {
         async fetchCurrency() {
-            const key = process.env.VUE_APP_FIXER;
-            const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=RUB,USD,EUR`);
+            const res = await fetch('https://api.openrates.io/latest?symbols=RUB,USD');
+            const json = await res.json();
 
-            return res.json();
+            json.rates.EUR = 1;
+
+            return json;
         },
     },
 };
